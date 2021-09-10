25a47c2a2e3d020842da1fc398901cde1c06a80e
Austin Hill <3822ah@gmail.com>
2021-09-10 16:00:55 +0100

    Switched to vscode on linux

--- PRNGs/RANDU.cpp
+++ PRNGs/RANDU.cpp
@@ -1,10 +1,10 @@
 #include "PRNGs.h"
 
-uint32_t lcg_state = std::random_device{}();
-uint32_t a = 65539;
+uint32_t randu_state = std::random_device{}();
+uint32_t ar = 65539;
 uint32_t m = 2147483648;
 
 uint32_t next_RANDU() {
-	lcg_state = (lcg_state * a) % m;
-	return lcg_state;
+	randu_state = (randu_state * ar) % m;
+	return randu_state;
 }
